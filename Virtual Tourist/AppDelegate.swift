ab456f50e155394f673d8733f603ca67b07455f0
Mohammad Awwad <msa492@gmail.com>
2015-09-13 20:32:29 +0300

    File: adding AppDelegate

---
+++ Virtual Tourist/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Virtual Tourist
+//
+//  Created by Mohammad Awwad on 9/9/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+  
+
+}
+
